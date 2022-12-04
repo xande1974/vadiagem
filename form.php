a42626e4016efe4371e475c982324a349aaa5178
xande1974 <xandegalatico@hotmail.com>
2022-12-04 15:19:56 -0300

    boas festas

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formularios em php</title>
+    <style>
+        body {
+            background-color: pink;
+        }
+    </style>
+</head>
+
+<body>
+    <form action="valor.php" method="get">
+        valor: <input type="number" name="v" />
+        <input type="submit" value="calcular raiz" />
+    </form>
+
+</body>
+
+</html>
